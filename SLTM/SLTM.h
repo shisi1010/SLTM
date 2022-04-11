d56a9b7a02d39dedec68fcbb97104314cef12cc6
shisi1010 <shisi1010@outlook.com>
2022-04-11 14:42:18 +0800

    添加项目文件。

---
+++ SLTM/SLTM.h
@@ -0,0 +1,32 @@
+﻿
+// SLTM.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CSLTMApp:
+// 有关此类的实现，请参阅 SLTM.cpp
+//
+
+class CSLTMApp : public CWinApp
+{
+public:
+	CSLTMApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CSLTMApp theApp;
